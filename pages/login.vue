d1913b639895035adc5961ba808c68d520e1f913
Felix Koschmidder <felix.koschmidder@allianz.de>
2020-01-18 16:21:23 +0100

    chore(*): inital commit

---
+++ pages/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <h1>Login</h1>
+
+    <UserAuthForm buttonText="Login" :submitForm="loginUser" />
+  </v-container>
+</template>
+
+<script>
+import UserAuthForm from "@/components/UserAuthForm";
+export default {
+  components: {
+    UserAuthForm
+  },
+  methods: {
+    loginUser(loginInfo) {
+      // choose strategy here
+      this.$auth.loginWith("local", {
+        data: loginInfo,
+      });
+      /* alert('You pressed a button') */
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/register.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <h1>Register</h1>
+
+    <UserAuthForm buttonText="Register" :submitForm="loginUser" hasName="true"/>
+  </v-container>
+</template>
+
+<script>
+import UserAuthForm from "@/components/UserAuthForm";
+export default {
+  components: {
+    UserAuthForm
+  },
+  methods: {
+    loginUser(loginInfo) {
+      debugger
+      alert('You pressed a button')
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
